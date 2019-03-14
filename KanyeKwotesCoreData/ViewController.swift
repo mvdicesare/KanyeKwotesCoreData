04dd5d1f1466cf18c7feeca1b00307730f0ef3e2
Cody <codyadcock10@gmail.com>
2019-03-14 13:49:49 -0600

    Initial Commit

---
+++ KanyeKwotesCoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KanyeKwotesCoreData
+//
+//  Created by Cody on 3/14/19.
+//  Copyright © 2019 Cody Adcock. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
